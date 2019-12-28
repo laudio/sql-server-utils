d3f78dc855f40f8e7f6da0002fe285d55ccaf273
Safal Pandey <safal.pandey.sp@gmail.com>
2019-12-28 14:10:21 +0545

    Create util to get surrounding lines in a message

---
+++ function/utils/get_surrounding_lines.sql
@@ -0,0 +1,30 @@
+--
+-- Procedure to get surrounding lines from a message.
+--
+CREATE FUNCTION utils.get_surrounding_lines(
+  @message NVARCHAR(MAX),
+  @line_number INT,
+  @adjacent_line_count INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  DECLARE @snippet NVARCHAR(MAX) = '';
+
+  DECLARE @line_table TABLE (
+    line_number INT IDENTITY,
+    line_text NVARCHAR(MAX)
+  );
+
+  INSERT INTO @line_table (line_text)
+  SELECT value FROM STRING_SPLIT(@message, N'
+');
+
+  SELECT
+    @snippet = @snippet + N'
+' + line_text
+  FROM @line_table
+  WHERE line_number BETWEEN @line_number - @adjacent_line_count AND @line_number + @adjacent_line_count;
+
+  RETURN @snippet;
+END
