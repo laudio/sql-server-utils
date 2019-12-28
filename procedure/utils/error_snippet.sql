60700049dd95fe7c8f3a3e2efc3b8f8b3c9b36b2
Safal Pandey <safal.pandey.sp@gmail.com>
2019-12-28 14:28:26 +0545

    Create error_snippet procedure in utils that gets the snippet of code where error was encountered

---
+++ procedure/utils/error_snippet.sql
@@ -0,0 +1,26 @@
+--
+-- Procedure to get snippet of procedure / function where error was encountered.
+--
+CREATE PROCEDURE utils.error_snippet
+  @proc_name NVARCHAR(255),
+  @line_number INT,
+  @adjacent_line_count INT = 2
+AS
+BEGIN
+
+  DECLARE @proc_text TABLE (
+    query NVARCHAR(MAX)
+  );
+
+  INSERT INTO @proc_text (query)
+  EXEC sp_helptext @proc_name;
+
+  DECLARE @query NVARCHAR(MAX) = ( SELECT STRING_AGG(query, '') FROM @proc_text);
+
+  DECLARE @snippet NVARCHAR(MAX) = utils.get_surrounding_lines(@query, @line_number, @adjacent_line_count);
+
+  PRINT 'ERRORED QUERY SNIPPET:' + CHAR(13) + @snippet;
+
+  SELECT @SNIPPET;
+
+END
